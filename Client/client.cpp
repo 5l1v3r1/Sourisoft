673d6577f1fca3d554e60bb7e38a6b65215e3ec2
Kevin Hascoet <neolex@email.com>
2015-11-09 14:39:37 +0100

    Ajout du client

---
+++ Client/client.cpp
@@ -0,0 +1,102 @@
+/**
+*     Sourisoft's client
+*
+*     @author Kevin Hascoët
+*
+      gcc client.cpp -o client -Wall
+*
+*/
+
+
+// if we're on windows
+#if defined (WIN32)
+	#include <windows.h>
+ 
+    #include <winsock2.h>
+	#pragma comment(lib, "ws2_32.lib")
+   typedef int socklen_t;
+
+// else, if we're on Linux
+#elif defined (linux)
+
+	#include <sys/types.h>
+	#include <sys/socket.h>
+	#include <arpa/inet.h>
+	#include <unistd.h>
+
+#endif
+
+
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+
+#define PORT 4444
+#define BUFSIZE 256
+#define IP "127.0.0.1"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    // if we're on windows
+    #if defined (WIN32)
+        WSADATA WSAData;
+        WSAStartup(MAKEWORD(2,2), &WSAData);
+    #endif
+
+
+	int sock,len;
+	char buffer[BUFSIZE];
+	struct sockaddr_in server_address;
+
+	// Create the socket 
+	if((sock= socket(AF_INET,SOCK_STREAM,0))== -1)
+	{
+		perror("socket");
+		exit (EXIT_FAILURE);
+	}
+
+	// configuration of the connection
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr= inet_addr(IP);
+
+	// Try to connect 
+	while(connect(sock, (struct sockaddr*)&server_address,sizeof(server_address)) == -1)
+	{
+		perror("connect");
+		// if we're on windows
+		#if defined (WIN32)
+			Sleep(20000);
+		// else, if we're on Linux
+		#elif defined (linux)
+			sleep(20);
+		#endif
+	}
+	int val;
+    while(true)
+    {
+    	if((val=recv(sock,buffer,BUFSIZE,0))<0)
+        {
+            perror("recv");
+            exit (EXIT_FAILURE);
+        }
+        buffer[val]='\0';
+        cout<<"recu :"<<buffer<<endl;
+
+        string result = "I'M ROOT";
+        len = result.size();
+        if(send(sock,result.c_str(),len,0)!=len)
+        {
+            perror("write");
+            exit (EXIT_FAILURE);
+        }
+    }
+
+	return 0;
+}
--- Serveur de test/serveur.cpp
+++ Serveur de test/serveur.cpp
@@ -1,13 +1,25 @@
+/**
+*     server used for testing the client while developmnent
+*     
+*
+*     @author Kevin Hascoët
+*
+      gcc serveur.cpp -o serveur -Wall
+*
+*/
+
 #include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <string>
 
 #define PORT 4444
 
+#define BUFSIZE 256
 
 
 using namespace std;
@@ -20,19 +32,19 @@ int main(int argc,char *argv[])
     socklen_t addressRVlen;
     struct sockaddr_in addressClient;
     socklen_t addressClientlen;
-    char buf[256];
-   char name[256];
+    char buf[BUFSIZE];
+    char name[BUFSIZE];
     // Create the rendez vous socket
     if((sockRVfd = socket(AF_INET,SOCK_STREAM,0))==-1)
     {
         perror("socket");
-        return -1;
+        exit (EXIT_FAILURE);
     }
 
-    if(gethostname(name,256))
+    if(gethostname(name,BUFSIZE))
     {
         perror("gethostname");
-        return -1;
+        exit (EXIT_FAILURE);
     }
     cout<<"Je m'execute sur "<<name<<"."<<endl;
 
@@ -47,14 +59,14 @@ int main(int argc,char *argv[])
     if( bind(sockRVfd , (struct sockaddr*)&addressRV,addressRVlen) == -1 )
     {
         perror("bind");
-        return -1;
+        exit (EXIT_FAILURE);
     }
 
     // start listening
     if(listen(sockRVfd,10)==-1)
     {
         perror("listen");
-        return -1;
+        exit (EXIT_FAILURE);
     }
 
     // Waiting for client
@@ -64,22 +76,40 @@ int main(int argc,char *argv[])
     if(sockCLfd == -1)
     {
         perror("accept");
-        return -1;
+        exit (EXIT_FAILURE);
     }
+   struct sockaddr_storage addr;
+   socklen_t len;
+   char ipstr[INET6_ADDRSTRLEN];
 
-    // A client just arrives he is connected with sockCLfd
+   len = sizeof addr;
+   getpeername(sockCLfd, (struct sockaddr*)&addr, &len);
+
+   // deal with both IPv4 and IPv6:
+   if (addr.ss_family == AF_INET) {
+       struct sockaddr_in *s = (struct sockaddr_in *)&addr;
+    
+       inet_ntop(AF_INET, &s->sin_addr, ipstr, sizeof ipstr);
+   } else { // AF_INET6
+       struct sockaddr_in6 *s = (struct sockaddr_in6 *)&addr;
+       inet_ntop(AF_INET6, &s->sin6_addr, ipstr, sizeof ipstr);
+   }
+
+
+    // A client just arrives he is connected with sockCLfd0
     cout<<"Un client vient de se connecter."<<endl;
+    cout<<"Son IP est : "<<ipstr<<endl;
     // This is a server for test so it can treat only one client
     close(sockRVfd);
-
-    if(read(sockCLfd,buf,256)<0)
+/*
+    if(read(sockCLfd,buf,BUFSIZE)<0)
     {
         perror("read");
-        return -1;
+        exit (EXIT_FAILURE);
     }
-
+*/
     int val=0;
-    cout<<"Le client s'appelle "<<buf<<endl;
+  //  cout<<"Le client s'appelle "<<buf<<endl;
     while(true)
     {
         cout<<"Entrez la commande a executer"<<endl;
@@ -90,18 +120,19 @@ int main(int argc,char *argv[])
         }
         buffer = "sh:"+buffer;
         val = buffer.size();
-        if(write(sockCLfd,buffer.c_str(),val)!=val)
+        if(send(sockCLfd,buffer.c_str(),val,0)!=val)
         {
             perror("write");
-            return -1;
+            exit (EXIT_FAILURE);
         }
         cout<<"Command sent...\nwaiting for response..."<<endl;
     
-        if(read(sockCLfd,buf,256)<0)
+        if((val=recv(sockCLfd,buf,BUFSIZE,0))<0)
         {
             perror("read");
-            return -1;
+            exit (EXIT_FAILURE);
         }
+        buf[val]='\0';
         cout<<"Reponse:\n"<<buf<<endl;
 
     }
